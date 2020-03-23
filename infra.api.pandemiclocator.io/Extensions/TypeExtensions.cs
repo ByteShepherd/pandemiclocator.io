3d7ec39671f2e962281c56f8588290a07279d7cc
Luiz Fernando Tuschinski <luiz.tuschinski@hbsis.com.br>
2020-03-22 21:13:55 -0300

    CleanCode GetFriendlyName

--- infra.api.pandemiclocator.io/Extensions/TypeExtensions.cs
+++ infra.api.pandemiclocator.io/Extensions/TypeExtensions.cs
@@ -8,23 +8,26 @@ namespace infra.api.pandemiclocator.io.Extensions
     {
         public static string GetFriendlyName(this Type type)
         {
-            string friendlyName = type.Name;
-            if (type.IsGenericType)
+            var friendlyName = type.Name;
+            if (!type.IsGenericType)
             {
-                int iBacktick = friendlyName.IndexOf('`');
-                if (iBacktick > 0)
-                {
-                    friendlyName = friendlyName.Remove(iBacktick);
-                }
-                friendlyName += "<";
-                Type[] typeParameters = type.GetGenericArguments();
-                for (int i = 0; i < typeParameters.Length; ++i)
-                {
-                    string typeParamName = GetFriendlyName(typeParameters[i]);
-                    friendlyName += (i == 0 ? typeParamName : "," + typeParamName);
-                }
-                friendlyName += ">";
+                return friendlyName;
             }
+
+            var iBacktick = friendlyName.IndexOf('`');
+            if (iBacktick > 0)
+            {
+                friendlyName = friendlyName.Remove(iBacktick);
+            }
+
+            friendlyName += "<";
+            var typeParameters = type.GetGenericArguments();
+            for (var i = 0; i < typeParameters.Length; ++i)
+            {
+                var typeParamName = GetFriendlyName(typeParameters[i]);
+                friendlyName += (i == 0 ? typeParamName : "," + typeParamName);
+            }
+            friendlyName += ">";
 
             return friendlyName;
         }
