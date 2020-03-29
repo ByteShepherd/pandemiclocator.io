c5ef53f20f2079033e8b82798791a964b0918c91
Luiz Fernando Tuschinski <luiz.tuschinski@hbsis.com.br>
2020-03-29 18:22:46 -0300

    Doc

--- pandemiclocator.io.database/GeolocationService.cs
+++ pandemiclocator.io.database/GeolocationService.cs
@@ -14,6 +14,7 @@ namespace pandemiclocator.io.database
             _dynamoDbProvider = dynamoDbProvider;
         }
 
+        //Trying https://ourcodeworld.com/articles/read/1019/how-to-find-nearest-locations-from-a-collection-of-coordinates-latitude-and-longitude-with-php-mysql
         public async Task<PandemicLocation[]> GetReportsNearByAsync(PandemicLocation currentLocation)
         {
             throw new NotImplementedException();
